408b8358dc7fa3b0d843c159528bf0cc6becc973
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-24 12:37:55 +0300

    type narrowing

--- financeapp/src/App.tsx
+++ financeapp/src/App.tsx
@@ -2,22 +2,34 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './components/cardList/CardList';
 import Search from './components/search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 
 function App() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([])
+  const [serverError, setServerError] = useState<string>("")
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  }
 
-    const onClick = (event:SyntheticEvent) => {
-        console.log(event);
+  const onClick = async (event: SyntheticEvent) => {
+    const result = await searchCompanies(search)
+    if (typeof result === "string") {
+      setServerError(result);
     }
+    else if (Array.isArray(result.data)) {
+      setSearchResult(result.data)
+    }
+    console.log(searchResult);
+    
+  }
   return (
     <div className="App">
-      <Search onClick={onClick} search = {search} handleChange = {handleChange} />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
     </div>
   );
