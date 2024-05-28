5f87b0ede8d8eb6780a422a91782c711e9340760
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-28 18:33:58 +0300

    Income Statement

--- financeapp/src/pages/designGuide/DesignGuide.tsx
+++ financeapp/src/pages/designGuide/DesignGuide.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Table from "../../components/table/Table";
 import RatioList from "../../components/ratioList/RatioList";
-import { testIncomeStatementData } from "../../components/table/testData";
+import { TestDataCompany, testIncomeStatementData } from "../../components/table/testData";
 
 
 type Props = {};
-
+const data = TestDataCompany;
 const tableConfig = [
   {
     label: "Market Cap",
@@ -21,7 +21,7 @@ const DesignGuide = (props: Props) => {
         components of the app with brief instructions on how to use them.
       </h1>
       <RatioList data={testIncomeStatementData} config={tableConfig} />
-      <Table />
+      <Table config={tableConfig} data={data} />
       <h3>
         Table - Table takes in a configuration object and company data as
         params. Use the config to style your table.
