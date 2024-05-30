ec2c064e402b032a0f1419ebdee9d0e90b381cc4
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-30 13:24:53 +0300

    Cash Flow Statement 2

--- financeapp/src/components/portfolio/cardPortfolio/CardPortfolio.tsx
+++ financeapp/src/components/portfolio/cardPortfolio/CardPortfolio.tsx
@@ -1,6 +1,6 @@
-import React, { SyntheticEvent } from 'react'
-import DeletePorfolio from '../deletePorfolio/DeletePorfolio';
-import { Link } from 'react-router-dom';
+import React, { SyntheticEvent } from "react";
+import { Link } from "react-router-dom";
+import DeletePorfolio from "../deletePorfolio/DeletePorfolio";
 
 interface Props {
   portfolioValues: string;
@@ -9,16 +9,19 @@ interface Props {
 
 const CardPortfolio = ({ portfolioValues, onPortfolioDelete }: Props) => {
   return (
-
-
     <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-      <Link to={`/company/${portfolioValues}`} className="pt-6 text-xl font-bold">{portfolioValues}</Link>
+      <Link
+        to={`/company/${portfolioValues}/company-profile`}
+        className="pt-6 text-xl font-bold"
+      >
+        {portfolioValues}
+      </Link>
       <DeletePorfolio
         portfolioValue={portfolioValues}
         onPortfolioDelete={onPortfolioDelete}
       />
     </div>
-  )
-}
+  );
+};
 
-export default CardPortfolio
+export default CardPortfolio;
